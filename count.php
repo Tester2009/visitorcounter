b012b849a1810d4bac3a670619a770618d0ababf
alepcat1710 <sulakecorporation1232@gmail.com>
2014-01-29 09:39:06 +0800

    count.php

---
+++ count.php
@@ -0,0 +1,22 @@
+<?php
+/* Hello . This code written by Muhammad Aliff Muazzam bin Jaafar .
+*
+*  Feel free to checkout .
+*  Built with love <3 .
+*
+*  http://www.facebook.com/Tester2009
+*  https://github.com/alepcat1710
+*  http://www.twitter.com/mambj2009
+*  http://www.twitter.com/alepcat1710
+*  http://kamisukagodam.blogspot.com
+*  http://about.me/Tester2009
+*  sulakecorporation1232@gmail.com
+*  - Tester2009 -
+*
+*/
+
+
+require 'config.php';
+nowdetect(); // detect ip . send(query) to database . 
+
+?>
