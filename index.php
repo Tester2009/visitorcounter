7e6985a8bd2fd7ff855b1cc526db27257b2cc6e4
alepcat1710 <sulakecorporation1232@gmail.com>
2014-01-29 09:35:32 +0800

    index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/* Hello . This code written by Muhammad Aliff Muazzam bin Jaafar .
+*
+*  Feel free to checkout .
+*  Built with love <3 .
+*
+*  http://www.facebook.com/Tester2009
+*  https://github.com/alepcat1710
+*  http://www.twitter.com/mambj2009
+*  http://www.twitter.com/alepcat1710
+*  http://kamisukagodam.blogspot.com
+*  http://about.me/Tester2009
+*  sulakecorporation1232@gmail.com
+*  - Tester2009 -
+*
+*/
+require 'config.php';
+showResult(); // show result from database . query-ing .
+
+countStats(); // count visitor hit .
+?>
