df132bb78c5e2d06143bc273d1c5fa785d0fa442
alepcat1710 <sulakecorporation1232@gmail.com>
2014-01-29 09:36:30 +0800

    .SQL file for database

---
+++ counter.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `counter` (
+  `id` int(11) NOT NULL,
+  `ip` text NOT NULL,
+  `useragent` text NOT NULL,
+  `time` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
